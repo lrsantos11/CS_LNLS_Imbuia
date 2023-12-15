48f3adcd41451b34d42457a94b24c52884d8f830
GBelemBarbosa <107588056+GBelemBarbosa@users.noreply.github.com>
2023-12-15 08:59:29 -0300

    updates on several files

--- src/data_utils.jl
+++ src/data_utils.jl
@@ -10,7 +10,7 @@ nist_data_files = readdir("data/nist_data/")
 filter!(data -> occursin(".csv", data), nist_data_files)
 # for nist_csv_file in nist_data_files
 # end
-function plot_nist_data(nist_data_files)
+#=function plot_nist_data(nist_data_files)
     num_data = length(nist_data_files)
     plt = plot(layout=num_data, size=(800, 800))
     for (index, nist_csv_file) in enumerate(nist_data_files)
@@ -20,4 +20,4 @@ function plot_nist_data(nist_data_files)
         plot!(plt, nist_data_x, nist_data_y, label=nist_csv_file[1:end-4], legend=:topright, xlabel="Wavenumbers (cm-1)", ylabel="(micromol/mol)-1m-1 (base 10)", subplot=index)
     end
     return plt
-end
+end=#
--- tests/tests_utils.jl
+++ tests/tests_utils.jl
@@ -1,15 +1,15 @@
-function iDFTx(x:: Vector{Number}; n=n)
+function iDFTx(x:: Vector{<:Number}; n=1)
     n₂=round(Int, n/2)
     ω₁=[exp(2*π*im*x[m])/n for m=1:n]
 
     return reduce(hcat, [ω₁.^(k/n) for k=-n₂:n₂])
 end
 
-function get_normally_dist_data(t_data:: Vector{Number}, interferogram_data:: Vector{Number}, n:: Int64; iDFTx=iDFTx)
+function get_normally_dist_data(t_data:: Vector{<:Number}, interferogram_data:: Vector{<:Number}, n:: Int64; iDFTx=iDFTx)
     # n random samples randomly distributed
     sampleₙ        = sample(t_data, n)
     interferogramₙ = interferogram_data[sampleₙ] 
-    iDFTₙ          = iDFTx(sampleₙ)
+    iDFTₙ          = iDFTx(sampleₙ; n=n)
 
-    return [sampleₙ, interferogramₙ, iDFTₙ]
+    return sampleₙ, interferogramₙ, iDFTₙ
 end
