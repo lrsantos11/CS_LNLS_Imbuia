7bbc12413755bc45714cde2d31c38ffe272cbbd3
Luiz-Rafael Santos <lrsantos11@gmail.com>
2023-12-15 10:22:46 -0300

    using better Vandermonde approach to compute DFT

--- tests/tests_utils.jl
+++ tests/tests_utils.jl
@@ -1,8 +1,9 @@
 function iDFTx(x:: Vector{<:Number}; n=1)
-    n₂=round(Int, n/2)
+    n₂ = round(Int, n/2)
     ω(x) = exp(2*π*im*x/n)
     ω₁ = ω.(x)
-    return reduce(hcat, [ω₁.^(k/n) for k=-n₂:n₂])
+
+    return vander(ω₁, -n₂:n₂)
 end
 
 function get_normally_dist_data(t_data:: Vector{<:Number}, interferogram_data:: Vector{<:Number}, n:: Int64)
@@ -12,4 +13,25 @@ function get_normally_dist_data(t_data:: Vector{<:Number}, interferogram_data:: 
     iDFTₙ          = iDFTx(sampleₙ; n=n)
 
     return sampleₙ, interferogramₙ, iDFTₙ
-endß
+end
+
+
+function vander!(V::AbstractMatrix, v::AbstractVector, range_vander::UnitRange{Int64})
+    S = eltype(v)
+    m = length(v)
+    n = length(range_vander)
+    (m, n) == size(V) || throw(DimensionMismatch())
+    n₂ = div(n, 2)
+    med = range_vander[n₂+1] + n₂ + 1
+    V[:, med] .= ones(S,m)
+
+    for j in med+1:n
+        @inbounds V[:, j] = v .* V[:, j-1]
+    end
+    for j in med-1:-1:1
+        @inbounds V[:, j] = V[:, j+1] ./ v
+    end
+    return V
+end
+
+vander(x::AbstractVector, range) = vander!(Array{eltype(v)}(undef, length(v), n), x, range)
